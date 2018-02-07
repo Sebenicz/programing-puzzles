c2a3d8aa2eedefd2df9764915adab96b130cf06c
SebastianS <strycharz97@gmail.com>
2018-02-08 00:55:44 +0100

    add function retrunig list of possible steps to centre

--- day3.py
+++ day3.py
@@ -9,3 +9,20 @@ def check_square(pos):
     square= square // 2
     return square
         
+def get_list_of_steps_in_square(square):
+    steps = square * 2 
+    steps_list = []
+    half_steps = square
+    step = steps
+
+    for i in range(steps):
+        steps_list.append(step)
+        
+        if i < half_steps: 
+            step -= 1
+        elif i >= half_steps:
+            step += 1  
+
+    return steps_list
+
+
