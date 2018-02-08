ff5b01ca6de180f39943857db247c25f04cb174a
SebastianS <strycharz97@gmail.com>
2018-02-08 17:06:50 +0100

    add counting solution function and main

--- day4.py
+++ day4.py
@@ -4,8 +4,33 @@ def get_table(filename):
         lines = f.readlines()  
 
     for i in range(len(lines)):
-        lines[i] = lines[i].split(" ")
+        lines[i] = lines[i].replace("\n", "").split(" ")
     
     return lines
 
+def count_solution_1():
+    table = get_table("day4_file.txt")
+    
+    solution=0
 
+    for line in table:
+
+        if chceck_repets(line):
+            solution += 1
+
+    return solution
+
+def chceck_repets(pasw):
+
+    for word in pasw:
+    
+        if pasw.count(word) != 1:
+            return False
+    
+    return True
+
+def main():
+    print("solution to part 1: " + str(count_solution_1()))
+
+if __name__=='__main__':
+    main()
