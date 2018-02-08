b14cc725536d703a9b7ef04928a2a43ac2f50059
SebastianS <strycharz97@gmail.com>
2018-02-08 18:01:47 +0100

    add solution for day5 part 1

--- day5.py
+++ day5.py
@@ -9,4 +9,22 @@ def get_table(filename):
 
     return lines
 
-#get_table("day5_file.txt")
+
+def jumping():
+    steps = get_table("day5_file.txt")
+    i = 0
+    jumps = 0
+    while True:
+        try:
+            steps[i] += 1
+            i = i + steps[i] 
+            i -= 1
+            jumps += 1
+            if i < 0:
+                raise IndexError
+        except IndexError:
+            print("out of loop in " + str(jumps) + " jumps.")
+            break
+            
+
+jumping()
